1516940f146812b6cfb400642f1d1e4de4d3a839
David Shams <david.shams@gmail.com>
2019-03-07 18:12:05 -0600

    bootstrap form tuned into React Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Signup from './components/signup/Signup';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h3>React Flux GraphQL</h3>
+       Sinpu
+       <Signup / >
       </div>
     );
   }
