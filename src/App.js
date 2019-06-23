75fac2c47d65549fe0cd0f924000630332a8ce57
David <david.shams@gmail.com>
2019-06-23 13:37:56 -0500

    bootsrap Navbar working fine

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Signup from './components/signup/Signup';
 import CKEditor from 'react-ckeditor-component';
 import Fragment from './components/fragment/Fragment';
+import Navbar from './components/navbar/Navbar';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
     const last = 'Shams';
     return (
       <div className="App">
+        <Navbar />
         <h3>React Flux GraphQL</h3>
         Sinpu
          <Fragment names={names} last={last}/>
